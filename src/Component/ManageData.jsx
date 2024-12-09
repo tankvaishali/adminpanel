b82ab94bd3e58a7e6f8ef799cc8422fe46a6b553
tankvaishali <vaishalitank28603@gmail.com>
2024-12-09 14:47:53 +0530

    changes designing

--- src/Component/ManageData.jsx
+++ src/Component/ManageData.jsx
@@ -45,33 +45,29 @@ function ManageData() {
         <div>
             <h2 className="text-center fw-bold pt-5 pb-3 pt-lg-0 pb-lg-0">Manage Data</h2>
             <div className="table-responsive py-0 px-0 py-lg-3">
-                <table className="table table-bordered table-striped">
-                    <thead className="thead-dark">
+                <table className="table table-bordered border-white table-striped">
+                    <thead className="thead-dark  tablecolor">
                         <tr className="text-center">
-                            <th className="bg-dark text-white" style={{ width: "5%" }}>No.</th>
-                            <th className="bg-dark text-white" style={{ width: "15%" }}>Picture</th>
-                            <th className="bg-dark text-white" style={{ width: "20%" }}>Tittle</th>
-                            <th className="bg-dark text-white" style={{ width: "30%" }}>Description</th>
-                            <th className="bg-dark text-white" style={{ width: "15%" }}>Date</th>
-                            <th className="bg-dark text-white" style={{ width: "15%" }}>Action</th>
+                            <th className="tablecolor  text-white" style={{ width: "5%" }}>No.</th>
+                            <th className="tablecolor  text-white" style={{ width: "15%" }}>Picture</th>
+                            <th className="tablecolor  text-white" style={{ width: "20%" }}>Tittle</th>
+                            <th className="tablecolor  text-white" style={{ width: "30%" }}>Description</th>
+                            <th className="tablecolor  text-white" style={{ width: "15%" }}>Date</th>
+                            <th className="tablecolor  text-white" style={{ width: "15%" }}>Action</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody className="border-transparent border-3 align-middle">
                         {data.map((item, index) => (
-                            <tr key={item.id} className="text-center">
+                            <tr key={item.id} className="text-center w-100">
                                 <td style={{ width: "5%" }}>{index + 1}</td>
                                 <td style={{ width: "15%" }}>
-                                    <a
-                                        href={item.imageUrl}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
+                                  
                                         <img
                                             src={item.imageUrl}
                                             alt="not found"
                                             style={{ width: "50px", height: "50px" }}
                                         />
-                                    </a>
+                                    
                                 </td>
                                 <td style={{ width: "20%" }}>{item.title}</td>
                                 <td style={{ width: "30%" }}>{item.description}</td>
